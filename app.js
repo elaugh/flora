0ec65b63756085ad83c7493789a0a3a1f43decdb
elaf <elaf@alliedmaldives.net>
2013-05-30 15:55:40 +0500

    page layout

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , gm = require('gm')
 
 var app = express();
 
@@ -17,7 +18,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
+  app.use(express.bodyParser({ keepExtensions: true, uploadDir: './public/images' }));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
@@ -27,9 +28,64 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+var file;
+
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/upload', function(req,res){
+  file = req.files.file.path;
+  res.json({
+    file : req.files.file.path.replace("public", "")
+  });
+});
+app.get('/filter/:filtername', function(req, res){
+  var filter = req.params.filtername;
+  var filters = {
+    jayz:function(){
+      gm(file)
+      .blur(.05)
+      .swirl(30)
+      .colorize(10,10,1)
+      .edge(7)
+      .stream()
+      .pipe(res)
+    },
+    gamma:function(){
+      gm(file)
+      .blackThreshold(100 ,20 ,50)
+      .stream()
+      .pipe(res)
+    },
+    blueivy:function(){
+      gm(file)
+      .colors(30)
+      .border(50, 50)
+      .emboss(.22)
+      .stream()
+      .pipe(res)
+    },
+    contrast:function(){
+      gm(file)
+      .monochrome()
+      .stream()
+      .pipe(res)
+    },
+    mirror:function(){
+      gm(file)
+      .flop()
+      .stream()
+      .pipe(res)
+    }
+  };
+  if(typeof filters[filter] == 'undefined'){
+    return res.json({error:"invalid filter"});
+  }
+  filters[filter]();
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+
